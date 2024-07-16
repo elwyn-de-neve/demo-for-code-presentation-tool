5c25a274788d181a840c13949e30ec8f28517e50
Elwyn <elwyn_de_neve@hotmail.com>
2024-07-16 10:20:17 +0200

    step 7

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ function App() {
   function fechtData(param) {
     console.log("fetching ", param);
   }
+
   return (
     <>
       <main>
-        // This will call the function immediately
+        {/* This will call the function immediately */}
         <button onClick={fechtData("data")}>Fetch Data</button>
       </main>
     </>
